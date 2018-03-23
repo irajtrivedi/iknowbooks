193eaa3cad27cd369530c50840bceccacdc47f74
Raj Trivedi <32588108+irajtrivedi@users.noreply.github.com>
2018-03-23 17:00:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('https');
@@ -109,9 +110,9 @@ function fetch_goodreads_review(bookName, callback) {
                     dataToSend = 'Great! Here are the details you requested';
                     dataToSend += '<br />Book Name: ' + result.GoodreadsResponse.book[0].title[0] + ' written by ' + result.GoodreadsResponse.book[0].authors[0].author[0].name[0];
                     dataToSend += '<br />Average rating for this book is ' + result.GoodreadsResponse.book[0].average_rating[0] + ' by ' + result.GoodreadsResponse.book[0].ratings_count[0] + ' users in goodreads.com';
-                    if (result.GoodreadsResponse.book[0].isbn[0] != undefined || result.GoodreadsResponse.book[0].isbn[0] != '')
+                    if (result.GoodreadsResponse.book[0].isbn[0] != undefined && result.GoodreadsResponse.book[0].isbn[0] != '')
                         dataToSend += '<br />You can buy this book at: ' + 'https://www.amazon.in/dp/' + result.GoodreadsResponse.book[0].isbn[0];
-                    else if (result.GoodreadsResponse.book[0].kindle_asin[0] != undefined || result.GoodreadsResponse.book[0].kindle_asin[0] != '')
+                    else if (result.GoodreadsResponse.book[0].kindle_asin[0] != undefined && result.GoodreadsResponse.book[0].kindle_asin[0] != '')
                         dataToSend += '<br />You can buy this book at: ' + 'https://www.amazon.in/dp/' + result.GoodreadsResponse.book[0].kindle_asin[0];
                     dataToSend += "<br /><br />Here's the brief about the book for you";
                     dataToSend += "<br />" + result.GoodreadsResponse.book[0].description[0];
